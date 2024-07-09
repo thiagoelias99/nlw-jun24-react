bfbc40724de5d98192592d12fc5d03451995d9c5
Thiago Elias <thiagoelias99@gmail.com>
2024-07-09 10:06:32 -0300

    feat: :sparkles: Create modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { ArrowRightIcon, CalendarIcon, MapPinIcon, Settings2Icon, UserRoundPlus } from 'lucide-react'
+import { ArrowRightIcon, AtSignIcon, CalendarIcon, MapPinIcon, PlusIcon, Settings2Icon, UserRoundPlus, XIcon } from 'lucide-react'
 import Image from 'next/image'
 import { useState } from 'react'
 
 export default function Home() {
   const [showUserInput, setShowUserInput] = useState(false)
+  const [showUserModal, setShowUserModal] = useState(false)
 
   return (
     <main className='flex h-svh flex-col justify-center items-center bg-pattern bg-no-repeat bg-center'>
@@ -23,7 +24,7 @@ export default function Home() {
             <MapPinIcon className='h-5 w-5 text-zinc-400' />
             <input
               disabled={showUserInput}
-              className='bg-transparent placeholder:text-zinc-400' type="text" placeholder="Para onde você vai?" />
+              className='bg-transparent placeholder:text-zinc-400 outline-none' type="text" placeholder="Para onde você vai?" />
           </div>
           <div className='flex justify-end items-center'>
             <div className='w-28 flex justify-end items-center gap-2'>
@@ -58,10 +59,12 @@ export default function Home() {
 
         {showUserInput && (
           <div className='w-[720px] mt-5 flex justify-start items-center gap-5 pl-6 pr-4 py-0 bg-zinc-900 h-16 rounded-xl shadow-shape'>
-            <div className='flex flex-1 justify-start items-center gap-2'>
+            <button
+              onClick={() => setShowUserModal(true)}
+              className='flex flex-1 justify-start items-center gap-2'>
               <UserRoundPlus className='h-5 w-5 text-zinc-400' />
-              <input className='bg-transparent placeholder:text-zinc-400' type="text" placeholder="Quem estará na viagem?" />
-            </div>
+              <span className='bg-transparent text-zinc-400 outline-none'>Quem estará na viagem?</span>
+            </button>
             <button className='bg-lime-300 hover:bg-lime-400 h-9 text-lime-950 rounded-lg flex items-center justify-center gap-2 px-5 py-2'>
               <p>Confirmar viagem</p>
               <ArrowRightIcon className='h-5 w-5' />
@@ -73,6 +76,33 @@ export default function Home() {
 
         <p className='w-full text-center text-zinc-500 text-sm mt-5'>Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
           com nossos <span className='text-zinc-300 underline'>termos de uso</span> e <span className='text-zinc-300 underline'>políticas de privacidade</span>.</p>
+      </div>
+
+
+      <div className={`inset-0 fixed bg-black w-full h-full bg-opacity-60 flex items-center justify-center ${showUserModal ? '' : 'hidden'}`}>
+        <div className='w-[640px] bg-zinc-900 flex flex-col justify-start items-start gap-5 px-6 py-5 rounded-xl'>
+          <div className='w-full'>
+            <div className='w-full flex justify-between items-center'>
+              <p className='text-lg text-white'>Selecionar convidados</p>
+              <XIcon
+                className='h-5 w-5 text-zinc-400 cursor-pointer'
+                onClick={() => setShowUserModal(false)}
+              />
+            </div>
+            <p className='text-sm text-zinc-400'>Os convidados irão receber e-mails para confirmar a participação na viagem.</p>
+          </div>
+          <div className='w-full h-0.5 bg-zinc-800'></div>
+          <div className='w-full flex justify-start items-center gap-5 pl-6 pr-4 py-0 bg-black h-16 rounded-xl shadow-shape'>
+            <div className='flex flex-1 justify-start items-center gap-2'>
+              <AtSignIcon className='h-5 w-5 text-zinc-400' />
+              <input className='bg-transparent placeholder:text-zinc-400 outline-none' type="text" placeholder="Digite o email do convidado" />
+            </div>
+            <button className='bg-lime-300 hover:bg-lime-400 h-9 text-lime-950 rounded-lg flex items-center justify-center gap-2 px-5 py-2'>
+              <p>Convidar</p>
+              <PlusIcon className='h-5 w-5' />
+            </button>
+          </div>
+        </div>
       </div>
     </main>
   )
