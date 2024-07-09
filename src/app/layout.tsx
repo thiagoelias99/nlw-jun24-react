2040c687a3b0e205b04cdfb3a8bcd95b30561921
Thiago Elias <thiagoelias99@gmail.com>
2024-07-09 09:30:27 -0300

    feat: :sparkles: Place & Date input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-zinc-950 text-zinc-50 antialiased`}>{children}</body>
     </html>
   )
 }
